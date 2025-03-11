e59cca2c11d2a587bd0dfbbcc7e2600db8207da5
Nutthapoom Suwanwitwaj <nutthapoom.suwanwitwaj@allianz.com>
2025-03-11 10:59:51 +0700

    add performance test script

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,56 +1,71 @@
 // Karma configuration file, see link for more information
+
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-chrome-launcher'),
-      require('karma-coverage'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-jasmine'),
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/squidex'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },
-      ],
-    },
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        // See /integration/README.md#browser-tests for more info on these args
-        flags: [
-          '--disable-dev-shm-usage',
-          '--disable-gpu',
-          '--headless',
-          '--no-sandbox',
+    config.set({
+        basePath: "",
+        frameworks: ["jasmine", "@angular-devkit/build-angular"],
+        plugins: [
+            require("karma-jasmine"),
+            require("@chiragrupani/karma-chromium-edge-launcher"),
+            require("karma-chrome-launcher"),
+            require("karma-spec-reporter"),
+            require("karma-jasmine-html-reporter"),
+            require("karma-sonarqube-reporter"),
+            require("karma-coverage"),
+            require("@angular-devkit/build-angular/plugins/karma"),
         ],
-      },
-    },
-    autoWatch: true,
-    browsers: ['Chrome'],
-    colors: true,
-    logLevel: config.LOG_INFO,
-    port: 9876,
-    reporters: ['progress', 'kjhtml'],
-    restartOnFileChange: true,
-    singleRun: false,
-  });
+        client: {
+            captureConsole: false,
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+            },
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+        },
+        jasmineHtmlReporter: {
+            suppressAll: true, // removes the duplicated traces
+        },
+        coverageReporter: {
+            dir: require("path").join(__dirname, "./coverage/integrateduw"),
+            subdir: ".",
+            reporters: [
+                { type: "lcov", subdir: "lcov-report" },
+                { type: "html", subdir: "html-report" },
+                { type: "text-summary" },
+            ],
+        },
+        reporters: ["progress", "kjhtml", "coverage", "spec", "sonarqube"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_ERROR,
+        singleRun: false,
+        restartOnFileChange: true,
+        autoWatch: true,
+        browsers: ["EdgeHeadlessNoSandbox"],
+        customLaunchers: {
+            EdgeHeadlessNoSandbox: {
+                base: "EdgeHeadless",
+                flags: [
+                    "--no-sandbox",
+                    "--disable-gpu",
+                    "--remote-debugging-port=9222",
+                    "--disable-site-isolation-trials",
+                ],
+            },
+            ChromeHeadlessNoSandbox: {
+                base: "ChromeHeadless",
+                flags: [
+                    "--no-sandbox",
+                    "--disable-gpu",
+                    "--disable-web-security",
+                    "--remote-debugging-port=9222",
+                    "--disable-site-isolation-trials",
+                ],
+            },
+        },
+    });
 };
---
+++ frontend/ngTest_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo 'start test-----------------------' >> execution_times.txt
+for i in {1..5}
+do
+  # Record the start time
+  start=$(date +%s%N)
+
+  # Execute the command
+ npm run test
+
+  # Record the end time
+  end=$(date +%s%N)
+
+  # Calculate the time difference
+  time_diff=$((end-start))
+
+  # Append the time difference to a file
+  echo $time_diff >> execution_times.txt
+done
+echo 'end test-----------------------' >> execution_times.txt
+
